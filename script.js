cb09e9b4934df9d6064ae8acf15b1b77d049e6bf
KennyD <iseceepcool2@gmail.com>
2019-03-06 16:20:16 +0100

    22

--- script.js
+++ script.js
@@ -86,10 +86,10 @@ netlifyIdentity.on('error', err => console.error('Error', err));
 netlifyIdentity.on('open', () => console.log('Widget opened'));
 netlifyIdentity.on('close', () => console.log('Widget closed'));
 
-/*var settings = {
+var settings = {
     "url": "https://todo-a4247d.appdrag.site/api/getTodo",
     "data": {
-        "userId" : netlifyIdentity.currentUser().id,
+        "userId" : user.id,
         "APIKey" : "b6c0a7d9-0566-44c1-a754-6c0f883bb2b5",
         "AD_PageNbr" : "1",
         "AD_PageSize" : "500"
@@ -101,4 +101,4 @@ netlifyIdentity.on('close', () => console.log('Widget closed'));
 };
 $.ajax(settings).done(function (response) {
     console.log(response);
-    });*/
+    });
