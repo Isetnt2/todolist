274373013b1441389e87ddca74db56312123b178
Isak Ahlberg <iseceepcool2@gmail.com>
2019-03-07 12:04:13 +0000

    23123213

--- script.js
+++ script.js
@@ -108,11 +108,8 @@ var get = function(userId){settings = {
 };
 $.ajax(settings).done(function (response) {
     console.log(response);
-    var html = JSON.parse(response.Table[0]);
-    var jsonHTML = json2html(html.todoData);
-    var todoJSON = response.Table[0].todoData;
+    var jsonHTML = json2html(response.Table[0].todoData);
     console.log(jsonHTML);
-    console.log(html);
     document.querySelector('.list-group').insertAdjacentHTML('beforeend', jsonHTML);
     updateClose();
   });
