cdef7f317f8a0671f537303634e2b205dfd5b9ac
Isak Ahlberg <iseceepcool2@gmail.com>
2019-03-09 09:02:50 +0000

    new APIkey

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ var get = function(userId){settings = {
     "url": "https://todo-a4247d.appdrag.site/api/getTodo",
     "data": {
         "userId" : userId,
-        "APIKey" : "296c2d24-168e-4105-97bb-e6668d4273b2,
+        "APIKey" : "296c2d24-168e-4105-97bb-e6668d4273b2",
         "AD_PageNbr" : "1",
         "AD_PageSize" : "500"
     },
